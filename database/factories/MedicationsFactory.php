13758f6438324b08361da2764e6269928397f7b9
Andre Vega <johan.vi.em.99@gmail.com>
2026-05-13 14:34:58 -0500

    se sube la estructura base con las tablas creadas y migradas

---
+++ database/factories/MedicationsFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Medications;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Medications>
+ */
+class MedicationsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/factories/PacientFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\pacient;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<pacient>
+ */
+class PacientFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
